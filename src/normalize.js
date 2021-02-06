bea7db138082b5e5e0a0bebe5f2bd46c11ab9805
Pawel Idczak <pawel.idczak@gmail.com>
2021-02-07 00:24:35 +0100

    prettier

--- src/normalize.js
+++ src/normalize.js
@@ -1,17 +1,15 @@
 const camelcase = require('camelcase')
 
-
 function removePrefix(prefix) {
   const prefixLength = prefix.length
   return (param) => param.slice(prefixLength)
-
 }
 
 function normalize(object, normalizers) {
   const newObject = {}
   for (const param in object) {
     let newParam = param
-    for(const normalizer of normalizers) {
+    for (const normalizer of normalizers) {
       newParam = normalizer(newParam)
     }
     newObject[newParam] = object[param]
