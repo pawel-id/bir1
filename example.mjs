56a4e1985296b7137d348893c2f9465c84c0f37e
Pawel Idczak <pawel.idczak@gmail.com>
2023-02-03 20:21:19 +0100

    refactor: rewrite example as esm module

---
+++ example.mjs
@@ -0,0 +1,11 @@
+import Bir from './dist/index.js'
+
+const bir = new Bir({ key: process.env.KEY })
+await bir.login()
+// console.log('StanDanych: ', await bir.value('StanDanych'))
+console.log('StatusUslugi:', await bir.value('StatusUslugi'))
+console.log('KomunikatUslugi:', await bir.value('KomunikatUslugi'))
+console.log('StatusSesji:', await bir.value('StatusSesji'))
+console.log(await bir.search({ regon: '012100784' }))
+console.log(await bir.search({ nip: '5261040567' }))
+console.log(await bir.report({ regon: '011417295', report: 'BIR11OsPrawna' }))
