3a16e9b185cf64eb6d8e3616ce9ae2b3dac79697
Pawel Idczak <pawel.idczak@gmail.com>
2024-01-05 23:56:29 +0100

    include data status in example

--- example.mjs
+++ example.mjs
@@ -2,7 +2,7 @@ import Bir from './dist/index.js'
 
 const bir = new Bir({ key: process.env.KEY })
 await bir.login()
-// console.log('StanDanych: ', await bir.value('StanDanych'))
+console.log('StanDanych: ', await bir.value('StanDanych'))
 console.log('StatusUslugi:', await bir.value('StatusUslugi'))
 console.log('KomunikatUslugi:', await bir.value('KomunikatUslugi'))
 console.log('StatusSesji:', await bir.value('StatusSesji'))
