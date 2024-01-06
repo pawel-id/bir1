9532158dea63eaf5a235f473c69169ae4478da5e
Pawel Idczak <pawel.idczak@gmail.com>
2024-01-06 01:11:03 +0100

    feat: define error codes

--- src/types.ts
+++ src/types.ts
@@ -9,6 +9,19 @@ export type GetValueOptions =
   | 'StatusUslugi'
   | 'KomunikatUslugi'
 
+/**
+ * Map of values returned by GetValue('KomunikatKod')
+ */
+export const KomunikatKodErrorCodes = {
+  '': 'Brak sesji. Sesja wygasła lub przekazano nieprawidłową wartość nagłówka sid.',
+  '0': 'Poprzednia operacja wykonana prawidłowo (dla sesji zgodnej z przesłanym sid !)',
+  '1': 'kod nieaktualny',
+  '2': 'Do metody DaneSzukaj przekazano zbyt wiele identyfikatorów.',
+  '4': 'Nie znaleziono podmiotów. (Częsta przyczyna: pNazwaRaportu dla P zamiast dla F i na odwrót).',
+  '5': 'Nieprawidłowa lub pusta nazwa raportu.',
+  '7': 'Brak sesji. Sesja wygasła lub przekazano nieprawidłową wartość nagłówka sid.',
+}
+
 export type DanePobierzPelnyRaportBir1Options =
   | 'PublDaneRaportFizycznaOsoba'
   | 'PublDaneRaportDzialalnoscFizycznejCeidg'
